897d3425937422319f9412fbd82eeb6eb03a5cfa
Dima Loktev <unknownliveid@hotmail.com>
2019-06-10 20:16:36 +0300

    Add xcode project

---
+++ ios/FullStackIOSClient/FullStackIOSClient/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FullStackIOSClient
+//
+//  Created by Dmitriy L on 6/10/19.
+//  Copyright © 2019 JetBrains. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
